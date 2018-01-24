c078762119fd79cd75cfc836c1c8626ba38fb287
Joseph W Becher <jwbecher@drazisil.com>
2018-01-24 14:37:36 -0500

    Corrected description and loaded version into footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,17 @@
 <template>
   <footer class="credits">
-    Created with Nuxt : <a class="orange" href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
+    {{ version }} Created with Nuxt : <a class="orange" href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
   </footer>
 </template>
+
+<script>
+import config from '../package.json'
+
+export default {
+  data () {
+    return {
+      version: config.version
+    }
+  }
+}
+</script>
